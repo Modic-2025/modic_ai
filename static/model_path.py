6c37b6a6754426c4ee90d2e1f395c0e37f15d176
Junho <darkenergy814@gmail.com>
2025-11-07 13:16:44 +0000

    Remove: Delete all files that don't use now.

--- static/model_path.py
+++
@@ -1,4 +0,0 @@
-vgg_path = "./experiments/vgg_normalised.pth"
-decoder_path = "./experiments/decoder_iter_160000.pth"
-Trans_path = "./experiments/transformer_iter_160000.pth"
-embedding_path = "./experiments/embedding_iter_160000.pth"
