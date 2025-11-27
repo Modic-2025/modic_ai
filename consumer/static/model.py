a483fb100819dc20803b34d2048c9abdafd2e8ab
Junho Kim <darkenergy814@gmail.com>
2025-11-27 14:40:10 +0900

    Fix: model 4.1로 수정

--- consumer/static/model.py
+++ consumer/static/model.py
@@ -2,5 +2,5 @@ import os
 
 
 API_KEY = os.getenv("OPENAI_API_KEY")
-MODEL = os.getenv("OPENAI_MODEL", "gpt-5-nano")
+MODEL = os.getenv("OPENAI_MODEL", "gpt-4.1")
 IMAGES_MODEL = os.getenv("OPENAI_IMAGE_MODEL", "gpt-image-1")
